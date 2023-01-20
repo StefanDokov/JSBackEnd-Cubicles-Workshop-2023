3566ffb6ebfefc598c6801a0c46d6c9795a23163
StefanDokov <stefandokov2211@gmail.com>
2023-01-20 19:03:53 +0200

    add views

--- src/index.js
+++ src/index.js
@@ -1,14 +1,13 @@
 
 const express = require('express');
 const config = require('./config');
+const routes = require('./routes');
 const setupViewEnigne = require('./config/viewEngine');
 
 const app = express();
 setupViewEnigne(app);
 
 app.use(express.static('src/public'));
-app.get('/', (req, res) => {
-    res.send('home page');
-});
+app.use(routes);
 
 app.listen(config.PORT, () => console.log(`Server is on port ${config.PORT}`));
