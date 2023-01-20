850b436f97ec6fb80549b2089bfe62d62f71cfd2
StefanDokov <stefandokov2211@gmail.com>
2023-01-20 19:09:28 +0200

    add home controller

---
+++ src/routes.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const Router = express.Router;
+const cubeController = require('./controllers/cubeController');
+const homeController = require('./controllers/homeController');
+
+const router = Router();
+
+router.get('/', homeController.getHomePage);
+router.get('/about', homeController.getAboutPage);
+router.get('/create', cubeController.getCreateCube);
+
+module.exports = router;
