9485fa4c41813f77be3d7e86d15887bae74c4885
StefanDokov <stefandokov2211@gmail.com>
2023-02-03 17:38:19 +0200

    same

--- src/routes.js
+++ src/routes.js
@@ -4,7 +4,7 @@ const cubeController = require('./controllers/cubeController');
 const homeController = require('./controllers/homeController');
 const accesoryController = require('./controllers/accessoryController');
 const authController = require('./controllers/authController');
-
+const {isAuthenticated} = require('./middlewares/authMiddleware');
 const router = Router();
 
 router.get('/', homeController.getHomePage);
@@ -13,8 +13,8 @@ router.get('/404', homeController.getErrorPage);
 
 router.use('/', authController);
 
-router.get('/cubes/create', cubeController.getCreateCube);
-router.post('/cubes/create', cubeController.postCreateCube);
+router.get('/cubes/create', isAuthenticated, cubeController.getCreateCube);
+router.post('/cubes/create', isAuthenticated, cubeController.postCreateCube);
 router.get('/cubes/:cubeId/details', cubeController.getDetails);
 router.get('/cubes/:cubeId/attach', cubeController.getAttachAccessory);
 router.post('/cubes/:cubeId/attach', cubeController.postAttachAccessory);
